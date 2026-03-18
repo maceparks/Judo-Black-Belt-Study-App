3aedb4037a3fc6f65bca4d5f223f4874b4505b72
maceparks <maceparks@gmail.com>
2026-03-17 21:13:09 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+﻿const cacheName = 'judo-v6';
+const assets = [
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+// Install the Service Worker and Cache Assets
+self.addEventListener('install', e => {
+  self.skipWaiting(); // Forces the waiting service worker to become active
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Clean up old caches when the new version activates
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Assets from Cache or Network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
